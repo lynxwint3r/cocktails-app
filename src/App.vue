e0246a8ed271a806ec8ddaaf934545b5fa0cb28f
lynxwint3r <dufour59.bastien@gmail.com>
2022-12-03 15:11:39 +0100

    initial commit + basic setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import AppLayout from '@/components/AppLayout.vue'
+
+import '@/assets/App.scss'
+</script>
+
+<template>
+  <AppLayout>Hello everyone!</AppLayout>
+</template>
+
+<style></style>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header class="header">Wanna drink something ?</header>
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
